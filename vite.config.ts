52623f7a86656d99c74c5dca0019878c7e893757
tiantingrui <976912095@qq.com>
2022-09-05 15:46:30 +0800

    feat: black vue3-project

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import { presetUno, presetAttributify, presetIcons } from "unocss";
-import Unocss from "unocss/vite";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-const rollupOptions = {
-  external: ["vue", "vue-router"],
-  output: {
-    globals: {
-      vue: "Vue",
-    },
-  },
-};
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    Unocss({
-      presets: [presetUno(), presetAttributify(), presetIcons()],
-    }),
-  ],
-  // 添加库模式配置
-  build: {
-    rollupOptions,
-    minify: false,
-    lib: {
-      entry: "./src/entry.ts",
-      name: "NoahUI",
-      fileName: "noah-ui",
-      // 导出模块格式
-      formats: ["es", "umd", "iife"],
-    },
-  },
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
-    },
-  },
-});
+  plugins: [vue()]
+})
